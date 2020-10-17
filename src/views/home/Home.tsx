f71b02b1972be88d76dd0b748b4a98794e8207c0
geo <giliadis.mage.guide>
2020-10-17 22:31:27 +0300

    First commit of adding bare repo to github

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,7 +1,49 @@
+import {
+  makeStyles,
+  Theme,
+  createStyles,
+  Grid,
+  Paper,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+      minHeight: 650,
+    },
+    title: {
+      margin: theme.spacing(3, 1),
+    },
+  })
+);
 
 interface Props {}
 
 export const Home = (props: Props) => {
-  return <div>Home</div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Typography variant="h2" className={classes.title}>
+        Welcome, username
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>Notes</Paper>
+        </Grid>
+
+        <Grid item sm={8}>
+          <Paper className={classes.paper}>Visits for today</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
