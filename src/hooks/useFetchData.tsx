88755eb8ae8fbb231e4a2f68ceeabab492d55984
georgeiliadis91 <george.iliadis.91@gmail.com>
2020-12-20 14:30:30 +0200

    fixed auth issue with backend

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -1,22 +1,24 @@
 import { useEffect, useState } from "react";
 import { useTriggerError } from "../redux/alert/hooks";
-import { useTriggerLoadingOn,useTriggerLoadingOff} from "../redux/loading/hooks";
+import {
+  useTriggerLoadingOn,
+  useTriggerLoadingOff,
+} from "../redux/loading/hooks";
 
-
-// FETCH DATA HOOK 
+// FETCH DATA HOOK
 
 const useFetchData = <T,>(
   fetchFunc: Promise<T[]>
 ): [T[], () => Promise<void>] => {
   const [data, setData] = useState<T[]>([]);
+
   const errorAlert = useTriggerError();
   const setLoadingON = useTriggerLoadingOn();
   const setLoadingOFF = useTriggerLoadingOff();
 
-
   const getData = async () => {
     setLoadingON();
-    
+
     try {
       const response = await fetchFunc;
       setData(response);
